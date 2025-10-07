fd7e563288556ad2c15e15968adc4d83b8a401cc
Sasha Fleming <alexander.fleming@rwth-aachen.de>
2025-10-07 10:54:51 +0200

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.56

---
+++ src/SimpleIntegration.jl
@@ -0,0 +1,5 @@
+module SimpleIntegration
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using SimpleIntegration
+using Test
+
+@testset "SimpleIntegration.jl" begin
+    # Write your tests here.
+end
